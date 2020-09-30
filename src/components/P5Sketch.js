347099ffd9c23331f302b8e52457741898fbfa07
Adam Butterworth <adamjbutterworth@gmail.com>
2020-09-29 20:51:20 -0400

    Import p5 dynamically to support gatsby build

--- src/components/P5Sketch.js
+++ src/components/P5Sketch.js
@@ -1,19 +1,3 @@
-import React, { useEffect, useRef } from "react"
-import { Flex, Box, Heading } from 'theme-ui'
-import CenterContainer from '../components/CenterContainer'
-import p5 from "p5"
-
-const P5Sketch = ({ sketch }) => {
-  const sketchEl = useRef(null);
-  useEffect(() => {
-    while (sketchEl.current.hasChildNodes()) {
-      sketchEl.current.removeChild(sketchEl.current.lastChild);
-    }
-    new p5(sketch, sketchEl.current)
-    /* return cleanup */
-  }, [sketch]);
-  return <div ref={sketchEl} />
-}
-
-
+import loadable from '@loadable/component'
+const P5Sketch = loadable(() => import('./LoadableP5Sketch'))
 export default P5Sketch;
